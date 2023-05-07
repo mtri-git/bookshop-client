049276920976e57d2f38df1c876b7628486a11db
mtri-git <vmtri20@gmail.com>
2023-05-07 20:59:35 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './redux/reducers'
-import { applyMiddleware } from 'redux'
+import { applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
+  compose;
+  
 const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
     applyMiddleware(thunk)
   ));
